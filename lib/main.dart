56f316f313f5c42420889acfe5005a18bdd60d93
Anshul Kataria <flunkeyit@gmail.com>
2019-12-28 17:55:02 +0530

    adding fonts and containers

--- lib/main.dart
+++ lib/main.dart
@@ -22,11 +22,45 @@ class MyApp extends StatelessWidget {
               Text(
                 'Joaquin Phoenix',
                 style: TextStyle(
-                  fontSize: 40,
+                  fontFamily: 'Montserrat',
+                  fontSize: 35,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
+                  letterSpacing: 1.5
                 ),
-              )
+              ),
+              Text(
+                'ACTOR/DIRECTOR',
+                style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontSize: 25,
+                  color: Colors.white,
+                  letterSpacing: 2.5
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(7),
+                color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                  child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.phone,
+                    size: 40,
+                    color: Colors.blueGrey,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    '+65 8111 6678',
+                    style: TextStyle(
+                        color: Colors.blueGrey,
+                        fontFamily: 'Montserret',
+                        fontSize: 20),
+                  )
+                ],
+              ))
             ],
           ),
         ),
